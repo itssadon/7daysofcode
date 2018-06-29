5f271667d80d6821a83b3c18f421d679a62889fa
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-06-30 00:47:54 +0100

    Finished basic functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+$(document).ready(function () {
+
+  String.prototype.capitalize = function (lower) {
+    return (lower ? this.toLowerCase() : this).replace(/(?:^|\s)\S/g, function (a) {
+      return a.toUpperCase();
+    });
+  };
+
+  $('select.dropdown').dropdown();
+  $('ui.checkbox').checkbox();
+
+  // Create sidebar on mobile and attach to menu open
+  $('.ui.sidebar').sidebar('attach events', '.toc.item');
+  $(".c_year").text(new Date().getFullYear());
+
+});
+
 let from = document.getElementById('from');
 let to = document.getElementById('to');
 let quantity = document.getElementById('quantity');
@@ -5,14 +22,14 @@ let result = document.getElementById('result');
 let conversion_of_one_unit = document.getElementById('conversion_of_one_unit');
 let currenciesList;
 
-//create an option containing a currency and append it in a select
+// Create an option containing a currency and append it in a select
 let createOption = (currency, target) => {
   let option = document.createElement('option');
   option.innerText = currency;
   target.appendChild(option);
 };
 
-//get all currencies and fill the two select elements as options
+// Get all currencies and fill the two select elements as options
 window.onload = () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
@@ -32,8 +49,8 @@ window.onload = () => {
 
 let conversionButton = document.getElementById('conversion');
 
-// a function that return the code of a country from its name (which will be taken from the select)
-// e.g "Tunisian Dinar" -> "TND"
+// A function that return the code of a country from its name (which will be taken from the select)
+// e.g "Nigerian Naira" -> "NGN"
 let getCodeFromName = name => {
   for (let currencyCode in currenciesList.results) {
     if (currenciesList.results[currencyCode].currencyName === name) {
@@ -42,7 +59,7 @@ let getCodeFromName = name => {
   }
 };
 
-//converts an amount of source currency to a target currency
+// Converts an amount of source currency to a target currency
 let conversion = (sourceName, targetName, number) => {
   let sourceCode = getCodeFromName(sourceName);
   let targetCode = getCodeFromName(targetName);
@@ -53,7 +70,7 @@ let conversion = (sourceName, targetName, number) => {
     .then(conversionObject => {
       let value = conversionObject.results[`${sourceCode}_${targetCode}`].val;
       result.innerText = value * number + targetCode;
-      conversion_of_one_unit.innerText = value + 'per unit';
+      conversion_of_one_unit.innerText = value + ' per unit';
     })
     .catch(error => console.log('error' + error));
 };
