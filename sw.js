b3727fa70c17144ff9902bf67e5be9ff1671e335
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-06-30 01:04:34 +0100

    Update urls to cache

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 let cacheName = 'cache-v1';
 let urlsToCache = [
   '/7daysofcode/',
-  '../index.html',
-  '../images/logo.png',
-  '../styles/themes/',
-  '../styles/main.css',
-  '../styles/pace.min.css',
-  '../styles/semantic.min.css',
-  '../scripts/jquery.min.js',
-  '../scripts/main.js',
-  '../scripts/pace.min.js',
-  '../scripts/semantic.min.js'
+  './index.html',
+  './images/logo.png',
+  './styles/themes/',
+  './styles/main.css',
+  './styles/pace.min.css',
+  './styles/semantic.min.css',
+  './scripts/jquery.min.js',
+  './scripts/main.js',
+  './scripts/pace.min.js',
+  './scripts/semantic.min.js'
 ];
 
 self.addEventListener('fetch', event => {
